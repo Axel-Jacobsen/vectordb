76c86ea1fb16f380baac437c4d334ad46c53906c
Axel Jacobsen <axelnj44@gmail.com>
2023-11-10 10:09:49 -0800

    fix import error + add __all__ stmt

--- vectordb/__init__.py
+++ vectordb/__init__.py
@@ -1,2 +1,5 @@
 # pylint: disable = line-too-long, trailing-whitespace, trailing-newlines, line-too-long, missing-module-docstring, import-error, too-few-public-methods, too-many-instance-attributes, too-many-locals
 
+from .memory import Memory
+
+__all__ = ("Memory",)
